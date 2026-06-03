28dc3db2583c0c711eccc314339de660adb1807f
Yuan Marquez <119801002+yam1yamiyam1@users.noreply.github.com>
2026-06-03 14:50:03 +0800

    revert

--- drills_41_50/drill_44.py
+++ drills_41_50/drill_44.py
@@ -2,7 +2,7 @@ import asyncio  # noqa: F401
 from typing import Callable  # noqa: F401
 
 # =========================================================================
-# SCENARIO: The Food Court
+# SCENARIO:
 # A food court has multiple stalls. Each stall is registered with a name
 # and a "preparer" function that takes a raw order string and returns a
 # prepared meal string.
